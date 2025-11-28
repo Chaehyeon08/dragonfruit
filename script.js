9803a3d52ae5f4eb9b0c4d99c608a70f794dcfb0
Chaehyeon08 <Chaehyeon.seo@stonybrook.edu>
2025-11-29 03:39:51 +0900

    pages

---
+++ script.js
@@ -0,0 +1,13 @@
+const sections = document.querySelectorAll("section, .hero, .page-section");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+sections.forEach(sec => observer.observe(sec));
